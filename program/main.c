aeb17dc34ace96c96a23330393b587e8e760ba68
blackpixel <blackpixel@protonmail.com>
2017-12-14 23:08:30 +0100

    Dodanie obsługi RTC z ekranem dotykowym

--- program/main.c
+++ program/main.c
@@ -15,6 +15,7 @@
  * Includes
  *****************************************************************************/
 #include "general.h"
+#include <stdio.h>
 #include <printf_P.h>
 #include <ea_init.h>
 #include <stdlib.h>
@@ -35,6 +36,8 @@
  *****************************************************************************/
 tU16 previousScore = 0;
 #define MENU_DELAY 1000
+#define INCREMENT 1
+#define DECREMENT 0
 /*
  * Enabled this define if you would like to test different values
  * for the LCD parameters. You will be able to input the values
@@ -88,188 +91,15 @@ lcd_color_t const COLORS_TAB[16] = {BLACK,
 /*****************************************************************************
  * Implementation of local functions
  ****************************************************************************/
-
 /*****************************************************************************
  *
  * Description:
- *    Sound arrays declaration
+ *    Images arrays declaration
  *
  ****************************************************************************/
-
-extern char wavSound[];
-
 extern unsigned short arkanoid_menu_mirror[];
 extern unsigned short arkanoid_settings_mirror[];
 extern unsigned short arkanoid_success_mirror[];
-
-tU32 wavSoundSize();
-                                 
-                                         /* TIMERS */
-static void delayMs(tU16 delayInMs)
-{
-  /*
-   * setup timer #1 for delay
-   */
-  T1TCR = 0x02;          //stop and reset timer
-  T1PR  = 0x00;          //set prescaler to zero
-  T1MR0 = delayInMs * (Fpclk / 1000);
-  T1IR  = 0xff;          //reset all interrrupt flags
-  T1MCR = 0x04;          //stop timer on match
-  T1TCR = 0x01;          //start timer
-  
-  //wait until delay time has elapsed
-  while (T1TCR & 0x01)
-    ;
-}
-static void udelay( unsigned int delayInUs )
-{
-  /*
-   * setup timer #1 for delay
-   */
-  T1TCR = 0x02;          //stop and reset timer
-  T1PR  = 0x00;          //set prescaler to zero
-  T1MR0 = (((long)delayInUs-1) * (long)Fpclk/1000) / 1000;
-  T1IR  = 0xff;          //reset all interrrupt flags
-  T1MCR = 0x04;          //stop timer on match
-  T1TCR = 0x01;          //start timer
-  
-  //wait until delay time has elapsed
-  while (T1TCR & 0x01)
-    ;
-}
-void setLed(int led, int on)
-{
-  tU8 commandString[] = {0x08, 0x00};
-  tU8 reg;
-
-  //adjudt address if LED >= 5
-  if (led >= 5)
-    commandString[0] = 0x09;
-
-    //read current register value
-  pca9532(commandString, 1, &reg, 1);
-
-
-  //update for new register value
-  switch (led)
-  {
-  case 1:
-  case 5:
-    reg &= ~0x03;
-    if (on == 1)
-      reg |= 0x01;
-    break;
-  case 2:
-  case 6:
-    reg &= ~0x0c;
-    if (on == 1)
-      reg |= 0x04;
-    break;
-  case 3:
-  case 7:
-    reg &= ~0x30;
-    if (on == 1)
-      reg |= 0x10;
-    break;
-  case 4:
-  case 8:
-    reg &= ~0xc0;
-    if (on == 1)
-      reg |= 0x40;
-    break;
-  default:
-    break;
-  }
-  commandString[1] = reg;
-  pca9532(commandString, sizeof(commandString), NULL, 0);
-}
-short* pData = NULL;
-  tU32 size = 0;
-  tU32 cnt = 0;
-  tU32 samples = 0;
-  tU32 numSamples;
-  tU32 sampleRate;
-  tU16 usDelay;
-  tU16 audioFlag = 1;
-  short soundCounter = 0;
-void audioPoint(){
-        DACR = 0x7fc0;
-        numSamples = 1200;
-        cnt++;
-        samples = 0;
-        usDelay = 1000000/ 24000 + 1;
-        samples = 0;
-        cnt = 11 + pData[8]/2;
-  
-        while(samples++ < numSamples)
-        {
-          tS32 val;
-      
-          val = pData[cnt++];
-          DACR = ((val + 0x7fff) & 0xffc0); // |  //actual value to output
-  
-          udelay(usDelay);
-        }
-}
-
-
-void playAudioPoint(Game *inputGame){
-  if(soundCounter==(inputGame->getScore(inputGame))-1){
-  	audioPoint();
-  	soundCounter++;
-  }
-}
-void setRTC(unsigned long *rtcValue, tU16 isAdd){
-	if(isAdd){
-		(*rtcValue)++;
-		printf("\n%d",*rtcValue);
-	}
-	else{
-		(*rtcValue)--;
-		printf("\n%d", *rtcValue);
-	}
-}
-/*void showRTC(){
-	lcd_putString(100, 160, "GAME OVER");
-	RTC_HOUR RTC_MIN RTC_SEC
-}*/
-/*****************************************************************************
- *
- * Description:
- *    Functions representing logic of the game
- *
- ****************************************************************************/
-void refreshScreen(Game* game){
-	int i = 0;
-	lcd_fillRect(game->racketPtr->getUpperRight(game->racketPtr).x, 
-				 game->racketPtr->getUpperRight(game->racketPtr).y,
-				 game->racketPtr->getLowerLeft(game->racketPtr).x, 
-				 game->racketPtr->getLowerLeft(game->racketPtr).y,
-				 game->racketPtr->getColor(game->racketPtr));
-    lcd_fillcircle(game->ballPtr->getCenter(game->ballPtr).x, 
-    			   game->ballPtr->getCenter(game->ballPtr).y, 
-    			   game->ballPtr->getRadius(game->ballPtr) + 5, 
-    			   BLACK);
-  	lcd_fillcircle(game->ballPtr->getCenter(game->ballPtr).x, game->ballPtr->getCenter(game->ballPtr).y, 
-  				   game->ballPtr->getRadius(game->ballPtr), game->ballPtr->getColor(game->ballPtr));
-  	for(i = 0; i < game->getNumOfBlocks(game); ++i)
-  	{
-  		if(game->blockPtr[i]->getIsDestroyed(game->blockPtr[i])){
-  			lcd_fillRect(game->blockPtr[i]->getUpperRight(game->blockPtr[i]).x,
-   						 game->blockPtr[i]->getUpperRight(game->blockPtr[i]).y,
-    					 game->blockPtr[i]->getLowerLeft(game->blockPtr[i]).x,
-    					 game->blockPtr[i]->getLowerLeft(game->blockPtr[i]).y,
-    					 BLACK);
-  		}
-  		else{
-  			lcd_fillRect(game->blockPtr[i]->getUpperRight(game->blockPtr[i]).x,
-   						 game->blockPtr[i]->getUpperRight(game->blockPtr[i]).y,
-    					 game->blockPtr[i]->getLowerLeft(game->blockPtr[i]).x,
-    					 game->blockPtr[i]->getLowerLeft(game->blockPtr[i]).y,
-    					 game->blockPtr[i]->getColor(game->blockPtr[i]));
-    	}
-    }
-}
 void lcdShowPicture(unsigned short *image){
 	  unsigned short* buffer = (unsigned short*) LCD_FRAME_BUFFER;
 	  int i, j, k=0;
@@ -282,13 +112,131 @@ void lcdShowPicture(unsigned short *image){
 		    }
 	  }
 }
-tU16 ifScored(Game *game)
+/*****************************************************************************
+ *
+ * Description:
+ *    Sound arrays declaration
+ *
+ ****************************************************************************/
+
+extern char wavSound[];
+
+tU32 wavSoundSize();
+                                         /* TIMERS */
+static void delayMs(tU16 delayInMs)
 {
-	if(game->getScore(game) != previousScore){
-		previousScore = game->getScore(game);
-		return 1;
+	/*
+	 * setup timer #1 for delay
+	 */
+	T1TCR = 0x02;          //stop and reset timer
+	T1PR  = 0x00;          //set prescaler to zero
+	T1MR0 = delayInMs * (Fpclk / 1000);
+	T1IR  = 0xff;          //reset all interrrupt flags
+	T1MCR = 0x04;          //stop timer on match
+	T1TCR = 0x01;          //start timer
+
+	//wait until delay time has elapsed
+	while (T1TCR & 0x01)
+		;
+}
+static void udelay( unsigned int delayInUs )
+{
+	/*
+	 * setup timer #1 for delay
+	 */
+	T1TCR = 0x02;          //stop and reset timer
+	T1PR  = 0x00;          //set prescaler to zero
+	T1MR0 = (((long)delayInUs-1) * (long)Fpclk/1000) / 1000;
+	T1IR  = 0xff;          //reset all interrrupt flags
+	T1MCR = 0x04;          //stop timer on match
+	T1TCR = 0x01;          //start timer
+
+	//wait until delay time has elapsed
+	while (T1TCR & 0x01)
+		;
+}
+short* pData = NULL;
+tU32 size = 0;
+tU32 cnt = 0;
+tU32 samples = 0;
+tU32 numSamples;
+tU32 sampleRate;
+tU16 usDelay;
+tU16 audioFlag = 1;
+short soundCounter = 0;
+
+void audioPoint(){
+	DACR = 0x7fc0;
+	numSamples = 1200;
+	cnt++;
+	samples = 0;
+	usDelay = 1000000/ 24000 + 1;
+	samples = 0;
+	cnt = 11 + pData[8]/2;
+
+	while(samples++ < numSamples){
+		tS32 val;
+		val = pData[cnt++];
+		DACR = ((val + 0x7fff) & 0xffc0); // |  //actual value to output
+		
+		udelay(usDelay);
 	}
-	return 0;
+}
+
+void playAudioPoint(Game *inputGame){
+	if(soundCounter==(inputGame->getScore(inputGame))-1){
+		audioPoint();
+		soundCounter++;
+	}
+}
+/*****************************************************************************
+ *
+ * Description:
+ *    LED functions
+ *
+ ****************************************************************************/
+void setLed(int led, int on)
+{
+	tU8 commandString[] = {0x08, 0x00};
+	tU8 reg;
+	//adjudt address if LED >= 5
+	if (led >= 5)
+		commandString[0] = 0x09;
+
+	//read current register value
+	pca9532(commandString, 1, &reg, 1);
+	//update for new register value
+	switch (led)
+	{
+	case 1:
+	case 5:
+		reg &= ~0x03;
+		if (on == 1)
+			reg |= 0x01;
+		break;
+	case 2:
+	case 6:
+		reg &= ~0x0c;
+		if (on == 1)
+			reg |= 0x04;
+		break;
+	case 3:
+	case 7:
+		reg &= ~0x30;
+		if (on == 1)
+			reg |= 0x10;
+		break;
+	case 4:
+	case 8:
+		reg &= ~0xc0;
+		if (on == 1)
+			reg |= 0x40;
+		break;
+	default:
+		break;
+	}
+	commandString[1] = reg;
+	pca9532(commandString, sizeof(commandString), NULL, 0);
 }
 void setLedScore(Game *game){
 	switch(game->getScore(game))
@@ -400,9 +348,114 @@ void setLedScore(Game *game){
 	setLed(7, 0);
 	setLed(8, 0);
 }
+/*****************************************************************************
+ *
+ * Description:
+ *    RTC functions
+ *
+ ****************************************************************************/
+void setRTC(volatile unsigned long *rtcValue, tU16 value){
+	if(value)
+		(*rtcValue)++;
+	else
+		(*rtcValue)--;
+}
+void setRtcHour(tU16 addValue){
+	if(addValue){
+		setRTC(&RTC_HOUR, INCREMENT);
+	}
+	else{
+		setRTC(&RTC_HOUR, DECREMENT);
+	}
+}
+void setRtcMin(tU16 addValue){
+	if(addValue){
+		if(RTC_MIN == 59){
+			setRtcHour(INCREMENT);
+			setRTC(&RTC_MIN, INCREMENT);
+		}
+		else
+			setRTC(&RTC_MIN, INCREMENT);
+	}
+	else{
+		if(RTC_MIN == 0){
+			setRtcHour(DECREMENT);
+			setRTC(&RTC_MIN, DECREMENT);
+		}
+		else
+			setRTC(&RTC_MIN, DECREMENT);
+	}
+}
+void setRtcSec(tU16 addValue){
+	if(addValue){
+		if(RTC_SEC == 59){
+			setRtcMin(INCREMENT);
+			setRTC(&RTC_SEC, INCREMENT);
+		}
+		else
+			setRTC(&RTC_SEC, INCREMENT);
+	}
+	else{
+		if(RTC_SEC == 0){
+			setRtcMin(DECREMENT);
+			setRTC(&RTC_SEC, DECREMENT);
+		}
+		else
+			setRTC(&RTC_SEC, DECREMENT);
+	}
+}
+
+
+/*void showRTC(){
+	lcd_putString(100, 160, "GAME OVER");
+	RTC_HOUR RTC_MIN RTC_SEC
+}*/
+/*****************************************************************************
+ *
+ * Description:
+ *    Functions drawing the program on the screen
+ *
+ ****************************************************************************/
+void refreshScreen(Game* game){
+	int i = 0;
+	lcd_fillRect(game->racketPtr->getUpperRight(game->racketPtr).x, 
+				 game->racketPtr->getUpperRight(game->racketPtr).y,
+				 game->racketPtr->getLowerLeft(game->racketPtr).x, 
+				 game->racketPtr->getLowerLeft(game->racketPtr).y,
+				 game->racketPtr->getColor(game->racketPtr));
+    lcd_fillcircle(game->ballPtr->getCenter(game->ballPtr).x, 
+    			   game->ballPtr->getCenter(game->ballPtr).y, 
+    			   game->ballPtr->getRadius(game->ballPtr) + 5, 
+    			   BLACK);
+  	lcd_fillcircle(game->ballPtr->getCenter(game->ballPtr).x, game->ballPtr->getCenter(game->ballPtr).y, 
+  				   game->ballPtr->getRadius(game->ballPtr), game->ballPtr->getColor(game->ballPtr));
+  	for(i = 0; i < game->getNumOfBlocks(game); ++i)
+  	{
+  		if(game->blockPtr[i]->getIsDestroyed(game->blockPtr[i])){
+  			lcd_fillRect(game->blockPtr[i]->getUpperRight(game->blockPtr[i]).x,
+   						 game->blockPtr[i]->getUpperRight(game->blockPtr[i]).y,
+    					 game->blockPtr[i]->getLowerLeft(game->blockPtr[i]).x,
+    					 game->blockPtr[i]->getLowerLeft(game->blockPtr[i]).y,
+    					 BLACK);
+  		}
+  		else{
+  			lcd_fillRect(game->blockPtr[i]->getUpperRight(game->blockPtr[i]).x,
+   						 game->blockPtr[i]->getUpperRight(game->blockPtr[i]).y,
+    					 game->blockPtr[i]->getLowerLeft(game->blockPtr[i]).x,
+    					 game->blockPtr[i]->getLowerLeft(game->blockPtr[i]).y,
+    					 game->blockPtr[i]->getColor(game->blockPtr[i]));
+    	}
+    }
+}
+tU16 ifScored(Game *game){
+	if(game->getScore(game) != previousScore){
+		previousScore = game->getScore(game);
+		return 1;
+	}
+	return 0;
+}
 void playGame(Game* game){
 	lcd_fillScreen(BLACK);
-	tU16 flag = 0;
 	refreshScreen(game);
     while(1)
 	{
@@ -443,50 +496,35 @@ void playGame(Game* game){
 	lcd_fillScreen(BLACK);
 	lcd_putString(100, 160, "GAME OVER");
 }
-void drawMenu(Game *game){
-	lcdShowPicture(arkanoid_menu_mirror);
+void drawSetTime(){
 	int x,y,z = 0;
+	tU16 getOut = 0;
+	char rtcString[20];
 	mdelay(MENU_DELAY);
 	while(1){
-		touch_xyz(&x, &y, &z);
-		if(25 < x && x < 210 && 170 < y && y < 225)
+		lcd_fillScreen(BLACK);
+		snprintf(rtcString, 20, "%2ld:%2ld:%2ld", RTC_HOUR, RTC_MIN, RTC_SEC);
+		lcd_putString(100, 160, rtcString);
+		while(1){
+			touch_xyz(&x, &y, &z);
+			if(0<x && x<120 && 0<y && y<200){
+				setRtcSec(INCREMENT);
+				mdelay(MENU_DELAY);
+				break;
+			}
+			if(180 < x && x < 240 && 0<y && y<200){
+				setRtcSec(DECREMENT);
+				mdelay(MENU_DELAY);
+				break;
+			}
+			if(0<x && x<240 && 200<y && y<320){
+				getOut = 1;
+				break;
+			}
+		}
+		if(getOut)
 			break;
-		if(26 < x && x < 208 && 256 < y && y < 288){
-			drawSettings(game);
-			mdelay(MENU_DELAY);
-			continue;
-		}
 	}
-}
-void drawSettings(Game *game){
-	lcdShowPicture(arkanoid_settings_mirror);
-	int x,y,z = 0;
-	mdelay(MENU_DELAY);
-	while(1){
-		touch_xyz(&x, &y, &z);
-		if(30<x && x<211 && 67 < y && y < 120){
-			drawSetTime();
-			drawSuccess();
-			mdelay(MENU_DELAY);
-			continue;
-		}
-		if(54 < x && x < 73 && 208 < y && y < 227){
-			game->racketPtr->setColor(game->racketPtr, RED);
-			drawSuccess();
-		}
-		if(105 < x && x < 124 && 208 < y && y < 227){
-			game->racketPtr->setColor(game->racketPtr, GREEN);
-			drawSuccess();
-		}
-		if(158 < x && x < 177 && 208 < y && y < 227){
-			game->racketPtr->setColor(game->racketPtr, BLUE);
-			drawSuccess();
-		}
-		if(34 < x && x < 210 && 258 < y && y < 286){
-			break;
-		}
-	}
-	lcdShowPicture(arkanoid_menu_mirror);
 }
 void drawSuccess(){
 	lcdShowPicture(arkanoid_success_mirror);
@@ -499,24 +537,48 @@ void drawSuccess(){
 	}
 	lcdShowPicture(arkanoid_settings_mirror);
 }
-void drawSetTime(){
+void drawSettings(tU16 *chosenColor){
+	lcdShowPicture(arkanoid_settings_mirror);
 	int x,y,z = 0;
-	char rtcString[20];
 	mdelay(MENU_DELAY);
 	while(1){
-		lcd_fillScreen(BLACK);
-		snprintf(rtcString, 20, "%2ld:%2ld:%2ld", RTC_HOUR, RTC_MIN, RTC_SEC);
-		lcd_putString(100, 160, rtcString);
-		while(1){
-			touch_xyz(&x, &y, &z);
-			if(0<x && x<120 && 0<y && y<320){
-			
-				break;
-			}
-			if(180 < x && x < 240 && 0<y && y<320){
-			
-				break;
-			}
+		touch_xyz(&x, &y, &z);
+		if(30<x && x<211 && 67 < y && y < 120){
+			drawSetTime();
+			drawSuccess();
+			mdelay(MENU_DELAY);
+			continue;
+		}
+		if(54 < x && x < 73 && 208 < y && y < 227){
+			*chosenColor = RED;
+			drawSuccess();
+		}
+		if(105 < x && x < 124 && 208 < y && y < 227){
+			*chosenColor = GREEN;
+			drawSuccess();
+		}
+		if(158 < x && x < 177 && 208 < y && y < 227){
+			*chosenColor = BLUE;
+			drawSuccess();
+		}
+		if(34 < x && x < 210 && 258 < y && y < 286){
+			break;
+		}
+	}
+	lcdShowPicture(arkanoid_menu_mirror);
+}
+void drawMenu(tU16 *chosenColor){
+	lcdShowPicture(arkanoid_menu_mirror);
+	int x,y,z = 0;
+	mdelay(MENU_DELAY);
+	while(1){
+		touch_xyz(&x, &y, &z);
+		if(25 < x && x < 210 && 170 < y && y < 225)
+			break;
+		if(26 < x && x < 208 && 256 < y && y < 288){
+			drawSettings(chosenColor);
+			mdelay(MENU_DELAY);
+			continue;
 		}
 	}
 }
@@ -529,9 +591,7 @@ void drawSetTime(){
  *    Always 0, since return value is not used.
  *
  ****************************************************************************/
- 
-int main(void)
-{
+int main(void){
 	//joystick
 	IODIR1 |= 0x38000000;
 	IOCLR1 |= 0x38000000;
@@ -556,97 +616,18 @@ int main(void)
 	tU8 initCommand[] = {0x12, 0x97, 0x80, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00};
 	pca9532(initCommand, sizeof(initCommand), NULL, 0);
 	//initialize printf()-functionality
-	// initialize external SDRAM
+	//initialize external SDRAM
 	sdramInit();
   
 	//print welcome message
 	printf("\n*********************************************************");
 	printf("\n*");
-	printf("\n* (C) 2008 Embedded Artists AB");
+	printf("\n* (C) 2017 Arkanoid - The Game");
 	printf("\n*");
-	printf("\n* Welcome to this program that demonstrates how to use the");
-	printf("\n* LCD QVGA.");
+	printf("\n* Welcome to this program that demonstrates simple game.");
 	printf("\n*");
 	printf("\n*********************************************************\n\n");
 
-#if 0  
-static tLcdParams ea_QVGA_v2 =
-{
-    28,        /* Horizontal back porch */
-    10,       /* Horizontal front porch */
-    2,       /* HSYNC pulse width */
-    240,      /* Pixels per line */
-    3,        /* Vertical back porch */
-    2,       /* Vertical front porch */
-    2,        /* VSYNC pulse width */
-    320,      /* Lines per panel */
-    0,        /* Do not invert output enable */
-    1,        /* Invert panel clock */
-    1,        /* Invert HSYNC */
-    1,        /* Invert VSYNC */
-    1,        /* AC bias frequency (not used) */
-    16,       /* Bits per pixel */
-    LCD_ADTFT,    /* LCD panel type */
-    0,        /* Single panel display */
-};
-#endif
-
-  
-#ifdef TEST_LCDPARAMS
-  
-	printf("Horizontal back porch (%d):\t", ea_QVGA_v2.hbp);
-	ea_QVGA_v2.hbp = consolGetIntNum();
-  
-	printf("Horizontal front porch (%d):\t", ea_QVGA_v2.hfp);
-	ea_QVGA_v2.hfp = consolGetIntNum();  
-  
-	printf("HSYNC pulse width (%d):\t\t", ea_QVGA_v2.hsw);
-	ea_QVGA_v2.hsw = consolGetIntNum();    
-  
-	printf("Pixels per line (%d):\t\t", ea_QVGA_v2.ppl);
-	ea_QVGA_v2.ppl = consolGetIntNum();      
-  
-	printf("Vertical back porch (%d):\t", ea_QVGA_v2.vbp);
-	ea_QVGA_v2.vbp = consolGetIntNum();        
-  
-	printf("Vertical front porch (%d):\t", ea_QVGA_v2.vfp);
-	ea_QVGA_v2.vfp = consolGetIntNum();  
-  
-	printf("VSYNC pulse width (%d):\t\t", ea_QVGA_v2.vsw);
-	ea_QVGA_v2.vsw = consolGetIntNum();    
-  
-	printf("Lines per panel (%d):\t\t", ea_QVGA_v2.lpp);
-	ea_QVGA_v2.lpp = consolGetIntNum();     
-  
-	printf("Invert ouput enable (%d):\t", ea_QVGA_v2.ioe);
-	ea_QVGA_v2.ioe = consolGetIntNum();       
-  
-	printf("Invert panel clock (%d):\t\t", ea_QVGA_v2.ipc);
-	ea_QVGA_v2.ipc = consolGetIntNum();   
-  
-	printf("Invert HSYNC (%d):\t\t", ea_QVGA_v2.ihs);
-	ea_QVGA_v2.ihs = consolGetIntNum();   
-  
-	printf("Invert VSYNC (%d):\t\t", ea_QVGA_v2.ivs);
-	ea_QVGA_v2.ivs = consolGetIntNum();   
-  
-  
-	printf("\n\nThe display will be initialized with the following values\n\n");  
-  
-	printf("Horizontal back porch:\t%d\n", ea_QVGA_v2.hbp);  
-	printf("Horizontal front porch:\t%d\n", ea_QVGA_v2.hfp); 
-	printf("HSYNC pulse width:\t%d\n", ea_QVGA_v2.hsw);  
-	printf("Pixels per line:\t%d\n", ea_QVGA_v2.ppl);
-	printf("Vertical back porch:\t%d\n", ea_QVGA_v2.vbp);
-	printf("Vertical front porch:\t%d\n", ea_QVGA_v2.vfp);
-	printf("VSYNC pulse width:\t%d\n", ea_QVGA_v2.vsw); 
-	printf("Lines per panel:\t%d\n", ea_QVGA_v2.lpp);
-	printf("Invert ouput enable:\t%d\n", ea_QVGA_v2.ioe);  
-	printf("Invert panel clock:\t%d\n", ea_QVGA_v2.ipc);
-	printf("Invert HSYNC:\t\t%d\n", ea_QVGA_v2.ihs); 
-	printf("Invert VSYNC:\t\t%d\n", ea_QVGA_v2.ivs);
-  
-#endif
 	//initialize LCD  
 
 	lcdInit(&ea_QVGA_v2); 
@@ -654,9 +635,12 @@ static tLcdParams ea_QVGA_v2 =
 	touch_init();
 	calibrateStart();
 	srand(RTC_SEC);
-	drawMenu(game);
 	tU16 numOfBlocks = 9 + rand() % 5;
+	tU16 chosenColor = PURPLE;
+	drawMenu(&chosenColor);
+	srand(RTC_SEC + RTC_MIN + RTC_HOUR);
 	Game *game = newGame(240, 320, numOfBlocks);
+	game->racketPtr->setColor(game->racketPtr, chosenColor);
 	playGame(game);
 	deleteGame(game);
 	return 0;
